0a22360bf5bc925d6d22e208f35222b569455f69
Shaik Nihal Afroze <nihal8722@gmail.com>
2025-10-02 17:50:44 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,4 @@
-// import express from 'express';
 
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import connectDB from './lib/db.js';
-// import cookieParser from 'cookie-parser';
-// import authRoutes from './routes/auth.route.js';
-// import appointmentRoutes from './routes/appointment.route.js';
-// import userRoutes from './routes/user.route.js';
-
-// const app = express();
-// app.use(express.json());
-// dotenv.config()
-// const allowedOrigins = ['http://localhost:5173', '']; // Add your allowed origins here
-// app.use(cors({
-//   origin: allowedOrigins,
-//   credentials: true
-// }));
-// app.use(cookieParser())
-
-
-
-// app.use("/api/auth",authRoutes)
-// app.use("/api/appointment",appointmentRoutes)
-// app.use("/api/user", userRoutes);
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, ()=>{
-//     console.log(`Server is running on port ${PORT}`);
-//     connectDB();
-// });
-
-// backend/src/index.js
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -58,11 +27,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = ['http://localhost:5173','https://book-my-doc-rose.vercel.app']; // dev: frontend url
+const allowedOrigins = ['http://localhost:5173','https://book-my-doc-rose.vercel.app']; 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cookieParser());
 
-// serve uploaded files (dev)
+
 app.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
 
 // API routes
